9d710468c3b2bdda57a2b2e311fd9a28b604b7d9
zavvdev <zaviryukha.igor99@gmail.com>
2025-05-26 22:30:17 +0300

    TX-7: Added allowOverBudget to mutate category request

--- src/app/requests/categories/mutateCategory.request.js
+++ src/app/requests/categories/mutateCategory.request.js
@@ -4,4 +4,5 @@ import { VALIDATION_MESSAGES as T } from "../../../infra/config.js";
 export var mutateCategoryRequestSchema = t.object({
   name: t.string().required(T.required).typeError(T.typeString),
   budgetLimit: t.number().min(0, T.minLength).nullable(),
+  allowOverBudget: t.boolean().default(false).typeError(T.typeBoolean),
 });
