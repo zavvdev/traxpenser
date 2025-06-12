54bc82a6509ee3f9c3eeae3554e77521bb09a648
zavvdev <zaviryukha.igor99@gmail.com>
2025-06-12 21:57:12 +0300

    TX-18: Added tests for POST /auth/login

--- __tests__/api/auth.test.js
+++ __tests__/api/auth.test.js
@@ -5,6 +5,7 @@ import { ROUTES } from "../../src/routes.js";
 import { LOGIN_CREDS } from "../config.js";
 import { assertErrorResponse, assertSuccessResponse } from "../utilities.js";
 import { MESSAGES } from "../../src/infra/config.js";
+import { expect } from "vitest";
 
 describe("Auth API", () => {
   describe("Register", () => {
@@ -16,9 +17,7 @@ describe("Auth API", () => {
     });
 
     it("should not register a user with existing username", async () => {
-      await request(app)
-        .post(ROUTES.auth.register())
-        .send(LOGIN_CREDS);
+      await request(app).post(ROUTES.auth.register()).send(LOGIN_CREDS);
 
       var res = await request(app)
         .post(ROUTES.auth.register())
@@ -27,6 +26,28 @@ describe("Auth API", () => {
       assertErrorResponse(res)({
         code: 409,
         message: MESSAGES.alreadyExists,
+      });
+    });
+  });
+
+  describe("Login", () => {
+    it("should login registered user", async () => {
+      await request(app).post(ROUTES.auth.register()).send(LOGIN_CREDS);
+      var res = await request(app).post(ROUTES.auth.login()).send(LOGIN_CREDS);
+
+      expect(res.body.data.token).toBeDefined();
+
+      assertSuccessResponse(res)({
+        message: MESSAGES.ok,
+      });
+    });
+
+    it("should not login unregistered user", async () => {
+      var res = await request(app).post(ROUTES.auth.login()).send(LOGIN_CREDS);
+
+      assertErrorResponse(res)({
+        code: 404,
+        message: MESSAGES.invalidCredentials,
       });
     });
   });
