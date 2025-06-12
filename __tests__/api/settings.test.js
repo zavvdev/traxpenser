db64738657e4ac201d0f4b24270873d6d6301bbc
zavvdev <zaviryukha.igor99@gmail.com>
2025-06-12 22:21:19 +0300

    TX-18: Added tests for settings

---
+++ __tests__/api/settings.test.js
@@ -0,0 +1,56 @@
+import { describe, it } from "vitest";
+import request from "supertest";
+import { app } from "../../src/index.js";
+import { ROUTES } from "../../src/routes.js";
+import {
+  assertErrorResponse,
+  assertSuccessResponse,
+  login,
+} from "../utilities.js";
+import { expect } from "vitest";
+import { CURRENCY, DEFAULT_SETTINGS } from "../../src/app/models/Settings.js";
+import { MESSAGES, VALIDATION_MESSAGES } from "../../src/infra/config.js";
+
+describe("Settings API", () => {
+  it("should get user settings", async () => {
+    var res = await request(app)
+      .get(ROUTES.settings.root())
+      .set("Authorization", await login());
+
+    expect(res.body.data.currency).toEqual(DEFAULT_SETTINGS.currency);
+  });
+
+  it("should update user settings", async () => {
+    var token = await login();
+
+    var res = await request(app)
+      .put(ROUTES.settings.root())
+      .send({ currency: CURRENCY.PLN })
+      .set("Authorization", token);
+
+    assertSuccessResponse(res)();
+
+    var settings = await request(app)
+      .get(ROUTES.settings.root())
+      .set("Authorization", token);
+
+    expect(settings.body.data.currency).toEqual(CURRENCY.PLN);
+  });
+
+  it("should not update user settings with invalid currency", async () => {
+    var token = await login();
+
+    var res = await request(app)
+      .put(ROUTES.settings.root())
+      .send({ currency: "asd" })
+      .set("Authorization", token);
+
+    assertErrorResponse(res)({
+      code: 400,
+      message: MESSAGES.validationError,
+      data: {
+        currency: VALIDATION_MESSAGES.invalid,
+      },
+    });
+  });
+});
